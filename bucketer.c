d7609d0515b03e5572f5d63002cf18b450bcf017
Naveen Kamagoud <136416909+naveenmk404@users.noreply.github.com>
2024-10-19 16:43:02 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -1,29 +1,76 @@
-#include <stdio.h>
-#include <assert.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-struct CountsBySoH {
-  int healthy;
-  int exchange;
-  int failed;
+class StateOfHealth{
+    public:
+        int present_capacity;
+        int rated_capacity;
+    
+    StateOfHealth(int pr_cap, int rat_cap){
+        present_capacity = pr_cap;
+        rated_capacity = rat_cap;
+    }
+    
+    double calSOH(int pr_capacity, int rat_capacity){
+        double temp = 100 * pr_capacity / (double) rat_capacity;
+        
+        return temp;
+    }
+    
 };
 
-struct CountsBySoH countBatteriesByHealth(const int* presentCapacities, int nBatteries) {
-  struct CountsBySoH counts = {0, 0, 0};
-  return counts;
-}
-
-void testBucketingByHealth() {
-  const int presentCapacities[] = {113, 116, 80, 95, 92, 70};
-  const int numberOfBatteries = sizeof(presentCapacities) / sizeof(presentCapacities[0]);
-  printf("Counting batteries by SoH...\n");
-  struct CountsBySoH counts = countBatteriesByHealth(presentCapacities, numberOfBatteries);
-  assert(counts.healthy == 2);
-  assert(counts.exchange == 3);
-  assert(counts.failed == 1);
-  printf("Done counting :)\n");
-}
-
-int main() {
-  testBucketingByHealth();
-  return 0;
+int main()
+{
+    int healthy=0, exchange=0, failed=0;
+    
+    vector<StateOfHealth> batteries;
+    
+    int n;
+    cout<<"Enter the number of batteries : ";
+    cin>>n;
+    
+    cout<<"Enter each battery present capacity followed by rated capacity "<<endl;
+    
+    for(int i=0;i<n;i++)
+    {
+        int pr_cap;
+        int rat_cap;
+        
+        cin>>pr_cap;
+        cin>>rat_cap;
+        
+        if(rat_cap==0){
+            cout<<"Rated capacity can't be zero "<<endl;
+            i--;
+            
+        }
+        else{
+            StateOfHealth battery = StateOfHealth(pr_cap, rat_cap);
+            batteries.push_back(battery);
+        }
+        
+    }
+    
+    
+    for(auto it : batteries)
+    {
+        double battery_soh = it.calSOH(it.present_capacity, it.rated_capacity);
+        
+        if(battery_soh>=80.0)
+        {
+            healthy++;
+        }
+        else if(battery_soh>=62.0)
+        {
+            exchange++;
+        }
+        else{
+            failed++;
+        }
+    }
+    
+    cout<<"Healthy batteries : "<<healthy<<endl;
+    cout<<"Exchange batteries : "<<exchange<<endl;
+    cout<<"Failed batteries : "<<failed<<endl;
+    return 0;
 }
